6002a9b7706044698dba910e90ec04299eab79a0
Rendoru <maulana.firman56@gmail.com>
2019-07-20 14:13:15 +0700

    Add more base type

--- base.go
+++ base.go
@@ -3,8 +3,14 @@ package suberror
 //RuntimeError a global error that is categorized as runtime error
 var RuntimeError = newBaseErrorType()
 
+//ClientError represent any error due to client input
+var ClientError = RuntimeError.Derive()
+
+//InternalError represent any error that happen because of server problem
+var InternalError = RuntimeError.Derive()
+
 //IOError represent any IO related error
-var IOError = RuntimeError.Derive()
+var IOError = InternalError.Derive()
 
 //NetworkError represent any network related error
 var NetworkError = IOError.Derive()
